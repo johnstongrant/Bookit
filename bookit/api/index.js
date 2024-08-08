9787aab407d3780734ac21a99f37701d392a0a00
Grant Johnston <grantjohnston02@gmail.com>
2024-08-08 13:05:48 -0700

    uncomment

--- bookit/api/index.js
+++ bookit/api/index.js
@@ -1,3 +1,3 @@
-// const { app } = require('@azure/functions');
-// const { ObjectId } = require('mongodb');
-// const mongoClient = require("mongodb").MongoClient;
+const { app } = require('@azure/functions');
+const { ObjectId } = require('mongodb');
+const mongoClient = require("mongodb").MongoClient;
